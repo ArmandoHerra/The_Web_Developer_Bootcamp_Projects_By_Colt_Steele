428a01618f9648e107f4636df240685d9dd6a4a4
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-01-16 16:02:20 -0600

    Modified index.html for lecture 94 and created loops.js

---
+++ Frontend/JavaScript/JavaScript Basics - Control Flow/loops.js
@@ -0,0 +1,25 @@
+/*jslint devel: true */
+
+var count = 1;
+
+while (count < 6) {
+    console.log("Count is: " + count);
+    count++;
+}
+
+var str = "hello";
+
+var count = 0;
+
+while (count < str.length) {
+    console.log(str[count]);
+    count++;
+}
+
+// Infinite Loop.
+
+//var count = 0;
+//
+//while (count < 10) {
+//    console.log(count);
+//}
