30bcecf1a21528ef2c694a4b8ac16e68ac43c65b
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-01-16 17:37:08 -0600

    Modified index.html and created for_loops.js for lecture 99.

---
+++ Frontend/JavaScript/JavaScript Basics - Control Flow/for_loops.js
@@ -0,0 +1,13 @@
+/*jslint devel: true */
+
+// Printing 1 - 5 with for loop.
+
+for (var count = 1; count < 6; count++) {
+    console.log(count);
+}
+
+var str = "hello";
+
+for (var i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
