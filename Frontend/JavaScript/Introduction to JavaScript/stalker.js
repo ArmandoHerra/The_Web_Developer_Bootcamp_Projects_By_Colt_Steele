aceb07443e4780efa3ebea2e3459a3474ee5077e
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-01-16 12:00:11 -0600

    Created stalker.js as proposed exercise from lecture 83.
    Modified index.html for the exercise.

---
+++ Frontend/JavaScript/Introduction to JavaScript/stalker.js
@@ -0,0 +1,11 @@
+/*jslint devel: true */
+
+var userFirstName = prompt("What Is You First Name?");
+
+var userLastName = prompt("What Is Your Last Name?");
+
+var userAge = prompt("What Is Your Age?");
+
+alert("Your full name is " + userFirstName + " " + userLastName);
+
+alert("Your age is " + userAge);
