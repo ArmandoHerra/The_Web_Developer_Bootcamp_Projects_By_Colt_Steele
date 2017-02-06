d00918f9e7ddca6774cc2c96f8c8fe0e5637e331
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-02-05 20:29:24 -0600

    Modified index.html and finished exercise from lecture 128.

---
+++ Frontend/JavaScript/JavaScript Basics - Objects/objects_quiz.js
@@ -0,0 +1,15 @@
+/*jslint devel: true */
+
+var someObject = {
+    
+    friends: [
+        {name: "Malfoy"},
+        {name: "Crabbe"},
+        {name: "Goyle"}
+    ],
+    
+    color: "Baby Blue",
+    isEvil: true
+};
+
+console.log(someObject.friends[0].name);
