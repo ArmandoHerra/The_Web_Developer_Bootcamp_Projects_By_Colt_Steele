75c5b00269359de850cabb6f1c6f0a87810b2fe0
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-02-06 18:36:30 -0600

    Modified index.html and created manipulating_content.js

---
+++ Frontend/JavaScript/DOM Manipulation/manipulating_content.js
@@ -0,0 +1,5 @@
+/*jslint devel: true */
+
+var paragraph = document.querySelector("p");
+paragraph.textContent;
+paragraph.textContent = "You have been Bamboozled! I changed the paragraphs content! :D";
