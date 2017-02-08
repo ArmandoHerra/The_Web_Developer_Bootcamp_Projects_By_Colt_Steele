55f6b6e1aca9c303f03623aa235a12ce7cf5bb83
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-02-07 22:08:56 -0600

    Completed the Lesson on Manipulating Content with DOM Manipulation.

--- Frontend/JavaScript/DOM Manipulation/manipulating_content.js
+++ Frontend/JavaScript/DOM Manipulation/manipulating_content.js
@@ -3,3 +3,5 @@
 var paragraph = document.querySelector("p");
 paragraph.textContent;
 paragraph.textContent = "You have been Bamboozled! I changed the paragraphs content! :D";
+var paragraph2 = document.querySelector("p");
+paragraph2.innerHTML = "<p>This is an <strong> innerHTML </strong> modification. </p>";
