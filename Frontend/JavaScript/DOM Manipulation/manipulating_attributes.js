f2163ea8885b77af2b06636ee656d5b3c215f823
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-02-08 09:55:54 -0600

    Added logo.png and corgi.jpg for the exercise on lecture 139. Also created manipulating_attributes.js

---
+++ Frontend/JavaScript/DOM Manipulation/manipulating_attributes.js
@@ -0,0 +1,9 @@
+/*jslint devel: true */
+
+var link = document.querySelector("a");
+link.getAttribute("href");
+link.setAttribute("href", "http://www.dogs.com");
+
+var img = document.querySelector("img");
+img.getAttribute("src");
+img.setAttribute("src", "corgi.jpg");
