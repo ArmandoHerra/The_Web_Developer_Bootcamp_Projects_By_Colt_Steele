2d93a385864f305c14a74eb3cf8dbe54e6abda5e
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-02-08 09:42:32 -0600

    Added comments to the file and removed an unnecessary line of code.

--- Frontend/JavaScript/DOM Manipulation/manipulating_content.js
+++ Frontend/JavaScript/DOM Manipulation/manipulating_content.js
@@ -1,7 +1,9 @@
 /*jslint devel: true */
 
+// Changing only text content with .textContent
 var paragraph = document.querySelector("p");
-paragraph.textContent;
 paragraph.textContent = "You have been Bamboozled! I changed the paragraphs content! :D";
+
+// Changing text and adding or modifiying HTML with .innerHTML
 var paragraph2 = document.querySelector("p");
 paragraph2.innerHTML = "<p>This is an <strong> innerHTML </strong> modification. </p>";
