a657340bdac55e3e93da16e95d367d2715004a80
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-02-12 15:42:28 -0600

    Modified index.html and created and finished selector_exercise.js for exercise on lecture 163.

---
+++ Frontend/jQuery/Intro to jQuery/selector_exercise.js
@@ -0,0 +1,6 @@
+/*jslint devel: true */
+
+$("div").css("background", "purple");
+$("div.highlight").css("width", "200px");
+$("div#third").css("border", "2px solid orange");
+$("div:first-of-type").css("color", "pink");
