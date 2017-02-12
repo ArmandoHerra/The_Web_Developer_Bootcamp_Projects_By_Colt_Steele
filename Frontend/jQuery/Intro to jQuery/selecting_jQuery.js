48ded3ed493fbbc2aa819e483d01926c5bf65d7a
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-02-12 15:18:55 -0600

    Created index.html, selecting_jQuery.js and jQuery for section 16.

---
+++ Frontend/jQuery/Intro to jQuery/selecting_jQuery.js
@@ -0,0 +1,21 @@
+/*jslint devel: true */
+
+$("body").css("backgroundColor", "#232323");
+
+var h1Styles = {
+    color: "white",
+    background: "steelblue",
+    margin: "30px",
+    padding: "30px",
+    textAlign: "center",
+    border: "3px solid white"
+};
+
+$("h1").css(h1Styles);
+
+var liStyles = {
+    color: "white",
+    fontSize: "20px"
+};
+
+$("li").css(liStyles);
