d598a872ac97237aa1ef726e6d5e24eedb71112b
Armando Alexis Herra Cortez <armandoherra369@gmail.com>
2017-02-14 15:27:20 -0600

    Added minor HTML and CSS changes. Major CSS overhaul.

--- Frontend/Todo List App/assets/js/todo_app.js
+++ Frontend/Todo List App/assets/js/todo_app.js
@@ -1,9 +1,11 @@
 /*jslint devel: true */
 
 // Check off specific todos by clicking.
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
